9423b79bb1c0967bcca6cbf47d8c389a3473de2c
林志翰 <linzhihan@linzhihandeMacBook-Pro.local>
2024-06-22 19:49:54 +0800

    added:輔助使用者上下移動箭頭

--- src/component/SideButton.jsx
+++ src/component/SideButton.jsx
@@ -1,7 +1,7 @@
 import { Icon } from "@iconify-icon/react";
 import React, { useState, useEffect, useRef } from "react";
 export default function SideButton({ top }) {
-  const buttonStyle = `fixed right-2 text-2xl  text-greydark`;
+  const buttonStyle = `fixed right-2 text-2xl  text-greydark transition-colors duration-300`;
   const activeScroll = (dir) =>
     scrollDirection === dir && isScrolling ? "text-primary" : "";
 
@@ -9,6 +9,13 @@ export default function SideButton({ top }) {
   const [isScrolling, setIsScrolling] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
   const scrollTimeout = useRef(null);
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  const scrollToBottom = () => {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+  };
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
@@ -24,17 +31,19 @@ export default function SideButton({ top }) {
     return () => {
       window.removeEventListener("scroll", handleScroll);
       if (scrollTimeout.current) clearTimeout(scrollTimeout.current);
-      scrollTimeout.current = setTimeout(() => setIsScrolling(false), 500);
+      scrollTimeout.current = setTimeout(() => setIsScrolling(false), 200);
     };
   }, [lastScrollY]);
 
   return (
     <>
       <Icon
-        className={`${buttonStyle} top-[180px] ${activeScroll("up")} `}
+        onClick={scrollToTop}
+        className={`${buttonStyle} top-[180px]  ${activeScroll("up")} `}
         icon="ph:arrow-up-bold"
       />
       <Icon
+        onClick={scrollToBottom}
         className={`${buttonStyle} bottom-[50px]
         
         ${activeScroll("down")}
