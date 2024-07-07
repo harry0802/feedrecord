ff3e80b6135b5ce9a66f4b2e3b9234a2c2c1c991
林志翰 <linzhihan@linzhihandeMacBook-Pro.local>
2024-07-07 19:04:06 +0800

    added: -package react-device-detec - 非行動裝置跳轉頁面

--- src/component/SideButton.jsx
+++ src/component/SideButton.jsx
@@ -1,7 +1,7 @@
 import { Icon } from "@iconify-icon/react";
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 export default function SideButton({ el }) {
-  const buttonStyle = `fixed right-2 text-2xl  text-greydark transition-colors duration-300 z-[1]`;
+  const buttonStyle = `fixed  right-2 text-2xl  text-greydark transition-colors duration-300 z-[1]`;
   const activeScroll = (dir) =>
     scrollDirection === dir && isScrolling ? "text-primary" : "";
   const [scrollDirection, setScrollDirection] = useState(null);
@@ -39,7 +39,7 @@ export default function SideButton({ el }) {
     <>
       <Icon
         onClick={scrollToTop}
-        className={`${buttonStyle} top-[${elHeight}]  ${activeScroll("up")} `}
+        className={`${buttonStyle} top-[${elHeight}] ${activeScroll("up")} `}
         icon="ph:arrow-up-bold"
       />
       <Icon
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,92 @@
 import { Outlet, Link, useLocation } from "react-router-dom";
+
+import { BrowserView, MobileView } from "react-device-detect";
+
 import FeedHeader from "../layout/FeedHeader.jsx";
 
 import { Icon } from "@iconify-icon/react";
+import { useEffect, useRef, useState } from "react";
+
+function App() {
+  return (
+    <>
+      <MobileView>
+        <MobileRootWrapper />
+      </MobileView>
+      <BrowserView>
+        <BrowserRootWrapper />
+      </BrowserView>
+    </>
+  );
+}
+function BrowserRootWrapper() {
+  const [remainSeconds, setRemainSeconds] = useState(15);
+  const intervalRef = useRef(null);
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setRemainSeconds((prev) => {
+        if (prev <= 1) {
+          clearInterval(intervalRef.current);
+          window.location.href = "https://www.twoshoulder.com/";
+        }
+        return prev - 1;
+      });
+    }, 1000);
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`flex place-items-center place-content-center  min-h-screen bg-greylight`}
+    >
+      <div
+        className={`w-[80%] sm:min-w-[40%] sm:max-w-[50%] min-h-[50vh] p-10 border  bg-white rounded-2xl flex flex-col justify-between  overflow-hidden text-center text-secondary `}
+      >
+        <div className={`text-xl  sm:text-3xl`}>
+          <h2>檢測到非行動設備</h2>
+        </div>
+
+        <div className={`mt-20 text-md sm:text-xl`}>
+          <p>
+            將於
+            {remainSeconds < 10 && remainSeconds > 0
+              ? `0${remainSeconds}`
+              : remainSeconds}
+            秒後跳轉至官方網站
+          </p>
+        </div>
+        <div className={`self-center mt-20  bg-primary text-white rounded-lg`}>
+          <a
+            href="https://www.twoshoulder.com/"
+            className={` inline-block py-2 px-10 lg:px-40  cursor-pointer`}
+          >
+            立即前往
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MobileRootWrapper() {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
+  return (
+    <div className="flex justify-center">
+      <div className="relative flex flex-col w-full max-w-[600px] min-h-screen bg-greylight">
+        <FeedHeader isHome={isHome} />
+        <main>
+          <Outlet />
+        </main>
+        <HomeButton isHome={isHome} />
+      </div>
+    </div>
+  );
+}
+
 function HomeButton({ isHome }) {
   return (
     !isHome && (
@@ -12,30 +97,6 @@ function HomeButton({ isHome }) {
         <Icon className="text-xl" icon="ion:home" />
       </Link>
     )
-  );
-}
-
-function RootWrapper({ children }) {
-  return (
-    <div className="flex justify-center">
-      <div className="relative flex flex-col w-full max-w-[600px] min-h-screen bg-greylight">
-        {children}
-      </div>
-    </div>
-  );
-}
-
-function App() {
-  const location = useLocation();
-  const isHome = location.pathname === "/";
-  return (
-    <RootWrapper>
-      <FeedHeader isHome={isHome} />
-      <main>
-        <Outlet />
-      </main>
-      <HomeButton isHome={isHome} />
-    </RootWrapper>
   );
 }
 
